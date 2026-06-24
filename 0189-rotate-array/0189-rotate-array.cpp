e53e75d4d4a028240eb0cc9d6df207a07bb82d39
Sonali Samal <sonalisamal7163@gmail.com>
2026-06-24 15:23:59 +0530

    Time: 0 ms (100%), Space: 30.6 MB (6.15%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n=nums.size();
+        k=k%n;
+        vector<int>ans(n);
+        for(int i=0; i<n ; i++)
+        {
+            int newindex=(i+k)%n;
+            ans[newindex]=nums[i];
+            
+        }
+        nums=ans;
+        
+    }
+
+};
